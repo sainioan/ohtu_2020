1c0b1b542fe46349d0fd1a7ae7c726e116dbd2db
sainioan <sainioa.anniina@gmail.com>
2020-11-19 23:02:45 +0200

    test tweak

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -18,7 +18,9 @@ import static org.mockito.Mockito.*;
  * @author anniinasainio
  */
 public class KauppaTest {
-    
+    Pankki pankki;
+    Varasto varasto;
+    Viitegeneraattori viite;
     public KauppaTest() {
     }
     
@@ -32,6 +34,9 @@ public class KauppaTest {
     
     @Before
     public void setUp() {
+        pankki = mock(Pankki.class);
+        varasto = mock(Varasto.class);
+        viite = mock(Viitegeneraattori.class);
     }
     
     @After
@@ -40,42 +45,23 @@ public class KauppaTest {
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
+  
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
-        // sitten testattava kauppa 
         Kauppa k = new Kauppa(varasto, pankki, viite);              
 
-        // tehdään ostokset
         k.aloitaAsiointi();
-        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
-        k.tilimaksu("pekka", "12345");
-
-        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455",5);   
-
+        k.lisaaKoriin(1);    
+        k.tilimaksu("pekka", "12345");  
+        
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
     }
     
         @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan2() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
+       
         when(viite.uusi()).thenReturn(43);
-
-        Varasto varasto = mock(Varasto.class);
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(2)).thenReturn(10); 
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "banaani", 2));
         when(varasto.saldo(3)).thenReturn(10); 
@@ -92,7 +78,7 @@ public class KauppaTest {
         k.tilimaksu("oliver", "55555");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto("oliver", 43, "55555", "33333-44455",6);   
+        verify(pankki).tilisiirto(eq("oliver"), eq(43), eq("55555"), eq("33333-44455"),eq(6));   
 
     }
     
