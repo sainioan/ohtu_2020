ce549d9b3c3237d92461d27db7a7b0c150c421c9
sainioan <sainio.anniina@gmail.com>
2020-12-03 16:52:11 +0200

    add classes to QueryLanguage

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -5,16 +5,24 @@ import statistics.matcher.*;
 public class Main {
     public static void main(String[] args) {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
-
+      
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
+//        Matcher m = new And( new HasAtLeast(5, "goals"),
+//                             new HasAtLeast(5, "assists"),
+//                             new PlaysIn("PHI")
+//        );
+//        
+//        for (Player player : stats.matches(m)) {
+//            System.out.println(player);
+//        }
         
-        for (Player player : stats.matches(m)) {
+        Matcher m = new And(new Not( new HasAtLeast(1, "goals") ), 
+        new PlaysIn("NYR")
+);
+            for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
+        
     }
 }
